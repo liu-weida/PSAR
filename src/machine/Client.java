2afedd422aeb5b3fd0398299bf91a76820abd651
LiXiaoquanSU <xiaoquan.li@etu.sorbonne-universite.fr>
2024-03-09 18:01:12 +0100

    annotation

--- src/machine/Client.java
+++ src/machine/Client.java
@@ -1,5 +1,6 @@
 package machine;
 
+import annotations.CommandMethod;
 import utils.message.ClientMessage;
 import utils.channel.Channel;
 import utils.message.Message;
@@ -32,20 +33,24 @@ public class Client implements Machine{
     @Override
     public void request(String methodType, List<Object> args) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
         for (Method method : getClass().getDeclaredMethods()) {
-            if (methodType.equals(method.getName())) {
-                if (method.getParameterTypes().length == args.size()) {
-                    method.setAccessible(true);
-                    try {
-                        method.invoke(this, args.toArray());
-                        return;
-                    } catch (IllegalArgumentException e) {
-                        throw new InvocationTargetException(e);
+            // 检查方法是否有@CommandMethod注解
+            if (method.isAnnotationPresent(CommandMethod.class)) {
+                if (methodType.equals(method.getName())) {
+                    if (method.getParameterTypes().length == args.size()) {
+                        method.setAccessible(true);
+                        try {
+                            method.invoke(this, args.toArray());
+                            return;
+                        } catch (IllegalArgumentException e) {
+                            throw new InvocationTargetException(e);
+                        }
                     }
                 }
             }
         }
-        throw new NoSuchMethodException("Method " + methodType + " with " + args.size() + " parameters not found.");
+        throw new NoSuchMethodException("Method " + methodType + " with " + args.size() + " parameters not found or not annotated with @CommandMethod.");
     }
+
 
 
     @Override
@@ -53,12 +58,12 @@ public class Client implements Machine{
 
     }
 
-    //
+    @CommandMethod
     private void dMalloc(String id) throws IOException, NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
         ClientMessage message = new ClientMessage("dMalloc", getId(), id);
         channel.send(message);
     }
-
+    @CommandMethod
     private int dAccessWrite(String id) throws IOException, ClassNotFoundException{
         ClientMessage message = new ClientMessage("dAccessWrite", getId(), id);
         channel.send(message);
@@ -70,7 +75,7 @@ public class Client implements Machine{
         }
         return 1;
     }
-
+    @CommandMethod
     // to do
     private int dAccessRead(String variableId) throws  IOException, ClassNotFoundException{
         ClientMessage message = new ClientMessage("dAccessRead", variableId);
@@ -83,12 +88,13 @@ public class Client implements Machine{
         }
         return 1;
     }
-
+    @CommandMethod
     private void dRelease(String variableId) throws IOException, ClassNotFoundException{
         ClientMessage message = new ClientMessage("dRelease", variableId);
         channel.send(message);
         ServerMessage serverMessage = (ServerMessage) channel.recv();
     }
+    @CommandMethod
     private void dFree(String id) throws IOException, ClassNotFoundException{
         ClientMessage message = new ClientMessage("dFree", id);
         channel.send(message);
