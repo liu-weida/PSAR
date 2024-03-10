8a0753f29d98693f1a8f3b5c383aa9d3a52c045e
LiXiaoquanSU <xiaoquan.li@etu.sorbonne-universite.fr>
2024-03-10 14:20:30 +0100

    更改serverMessage格式

---
+++ src/utils/message/MessageType.java
@@ -0,0 +1,5 @@
+package utils.message;
+public enum MessageType {
+    DMA, DAW, DAR, DRE, DFR, EXP; // 代表不同的命令类型，如dMalloc, dAccessWrite等
+}
+
--- src/utils/processor/ServerProcessor.java
+++ src/utils/processor/ServerProcessor.java
@@ -7,6 +7,8 @@ import utils.channel.Channel;
 import utils.channel.ChannelBasic;
 import utils.message.Message;
 import utils.message.ServerMessage;
+import utils.message.MessageType;
+import utils.message.OperationStatus;
 
 import java.io.IOException;
 import java.net.InetAddress;
@@ -50,12 +52,13 @@ public class ServerProcessor implements Processor{
                     case "dRelease" -> handleDRelease(channel);
                     case "dFree" -> handleDFree(variableId, channel);
                     default -> {
-                        message = new ServerMessage("respond", false, "Commande error");
+                        message = new ServerMessage(MessageType.EXP,OperationStatus.COMMAND_ERROR);
 
                     }
                 }
             }catch(IOException e){
                 //传输错误
+                //尝试使用镜像
                 throw new ServerException("传输错误",e);
             }
 
@@ -70,16 +73,16 @@ public class ServerProcessor implements Processor{
     private void handleDMalloc(String variableId, String clientId, Channel channel) throws IOException {
         System.out.println("收到初始化数据信息");
         if(server.variableExistsHeap(variableId)){
-            message = new ServerMessage("respondDMalloc",false,"dMalloc fail(data already exists)");
+            message = new ServerMessage(MessageType.DMA,OperationStatus.DATA_EXISTS);
         }else{
             try{
                 server.insertData(clientId, variableId);
             }catch(ServerException e){
-                ServerMessage message = new ServerMessage("respondDMalloc",false,"dMalloc fail(insert error)");
+                ServerMessage message = new ServerMessage(MessageType.DMA,OperationStatus.INSERT_ERROR);
                 channel.send(message);
                 return;
             }
-            message = new ServerMessage("respondDMalloc",true,"dMalloc success"); // 确保成功消息在try块内部
+            message = new ServerMessage(MessageType.DMA,OperationStatus.SUCCESS); // 确保成功消息在try块内部
 
         }
     }
@@ -89,25 +92,25 @@ public class ServerProcessor implements Processor{
         lock.writeLock().lock();
         //数据锁
         if(!server.variableExistsHeap(variableId)){
-            message = new ServerMessage("respondDAccessWrite",false,"dAccessWrite fail(data not exists)");
+            message = new ServerMessage(MessageType.DAW, OperationStatus.DATA_NOT_EXISTS);
             return;
         }
-        ServerMessage message = new ServerMessage("respondDAccessWrite",true,"dAccessWrite waiting for dRelease");
+        ServerMessage message = new ServerMessage(MessageType.DAW,OperationStatus.WAIT_FOR_DRE);
         channel.send(message);
         ClientMessage clientMessageWrite= (ClientMessage)channel.recv();
         if(!Objects.equals(clientMessageWrite.getCommand(), "dRelease")){
-            message = new ServerMessage("respondDAccessWrite",false,"dAccessWrite fail(command error)");
+            message = new ServerMessage(MessageType.DAW,OperationStatus.COMMAND_ERROR);
             //解锁下一个notifyone
         }else{
             try{//更新数据到list首位
                 server.deleteData(clientId, variableId);
                 server.insertData(clientId, variableId);
             }catch(ServerException e){
-                message = new ServerMessage("respondDAccessWrite",false,"dAccessWrite fail(insert error)");
+                message = new ServerMessage(MessageType.DAW,OperationStatus.INSERT_ERROR);
                 //解锁下一个notifyone
                 return;
             }
-            message = new ServerMessage("respondDAccessWrite",true,"dAccessWrite success");
+            message = new ServerMessage(MessageType.DAW,OperationStatus.SUCCESS);
 
             //解锁下一个notifyone
             lock.writeLock().unlock();
@@ -118,16 +121,16 @@ public class ServerProcessor implements Processor{
         //数据锁
         lock.readLock().lock();
         if (!server.variableExistsHeap(variableId)) {
-            message = new ServerMessage("respondDAccessRead", false, "dAccessRead fail(data not exists)");
+            message = new ServerMessage(MessageType.DAR,OperationStatus.DATA_NOT_EXISTS);
 
             //解锁下一个notifyone
             return;
         }
-        ServerMessage message = new ServerMessage("respondDAccessRead", true, "dAccessRead waiting for dRelease",clientPort,clientHost);
+        ServerMessage message = new ServerMessage(MessageType.DAR,OperationStatus.WAIT_FOR_DRE,clientPort,clientHost);
         channel.send(message);
         ClientMessage clientMessageRead = (ClientMessage) channel.recv();
         if (!Objects.equals(clientMessageRead.getCommand(), "dRelease")) {
-            message = new ServerMessage("respondDAccessRead", false, "dAccessRead fail(command error)");
+            message = new ServerMessage(MessageType.DAR,OperationStatus.COMMAND_ERROR);
 
             //解锁下一个notifyone
         } else {
@@ -135,12 +138,12 @@ public class ServerProcessor implements Processor{
                 server.deleteData(clientId, variableId);
                 server.insertData(clientId, variableId);
             } catch (ServerException e) {
-                message = new ServerMessage("respondDAccessRead",false,"dAccessRead fail(insert error)");
+                message = new ServerMessage(MessageType.DAR,OperationStatus.INSERT_ERROR);
 
                 //解锁下一个notifyone
                 return;
             }
-            message = new ServerMessage("respondDAccessRead", true, "dAccessRead success");
+            message = new ServerMessage(MessageType.DAR,OperationStatus.SUCCESS);
 
             //解锁下一个notifyone
             lock.readLock().unlock();
@@ -148,17 +151,17 @@ public class ServerProcessor implements Processor{
     }
     private void handleDRelease(Channel channel) throws IOException {
         System.out.println("数据使用完毕信息");
-        message = new ServerMessage("respondDRelease", false, "command error");
+        message = new ServerMessage(MessageType.DRE,OperationStatus.COMMAND_ERROR);
     }
     private void handleDFree(String variableId, Channel channel) throws IOException {
         System.out.println("收到客户端删除数据请求");
         //数据锁
         if (!server.variableExistsHeap(variableId)) {
-            message = new ServerMessage("respondDFree", false, "Dfree fail(data not exists");
+            message = new ServerMessage(MessageType.DFR,OperationStatus.DATA_NOT_EXISTS);
 
         } else {//如果存在
             server.deleteVariable(variableId);
-            message = new ServerMessage("respondDFree", true, "Dfree success");
+            message = new ServerMessage(MessageType.DFR,OperationStatus.SUCCESS);
 
             //notifyall
         }
